0974dbacb50492271f84956e87142e63734a0fdc
pawarpranav802 <97682404+pawarpranav802@users.noreply.github.com>
2022-02-20 12:25:28 +0530

    Add files via upload

---
+++ Phase 3/Practice Project/4 Webservices in java/RestApiOneApplication.java
@@ -0,0 +1,14 @@
+package com.test.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiOneApplication.class, args);
+    }
+
+}
+
